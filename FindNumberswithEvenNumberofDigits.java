4bcf796a0a88422a34c5d3dae84d7f5c30cc0963
trhthang <trhthang0401@gmail.com>
2024-01-13 14:57:06 +0900

    leetcode

---
+++ FindNumberswithEvenNumberofDigits.java
@@ -0,0 +1,36 @@
+class FindNumberswithEvenNumberofDigits {
+
+    public static int findNumbers(int[] nums){
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            //kiểm tra số chữ số của a
+            // Nếu số chữ số là chẵn ==> tăng biến đếm lên 1
+            if(tinhSoChuSo(nums[i]) % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int tinhSoChuSo(int a) {
+        int  kq = a ; 
+        int count = 0;
+        while(kq != 0) {
+            kq = kq/10;
+            count++;
+            a  = kq;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] c = {1233,22,33322,4123131,5123211};
+
+        System.err.println(FindNumberswithEvenNumberofDigits.findNumbers(c));
+
+        System.out.println(tinhSoChuSo(123123123));
+
+    }
+}
+ 
